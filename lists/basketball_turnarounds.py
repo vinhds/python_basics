41db023fb315713442c0263bfb0911af96c319c0
VinhD <102884144+vinhds@users.noreply.github.com>
2023-03-19 12:25:44 -0500

    add problems about lists

---
+++ lists/basketball_turnarounds.py
@@ -0,0 +1,16 @@
+#In a basketball match, we know how many points both Team A and Team B have scored and the exact second when it happened. Within a second, it will not be possible to score more than one point. King James is observing the task input and wants to answer the following two questions:
+#
+#    How many points have been scored during the first half-time, that is in the first half of the game, if we know that the entire game lasts 4 × 12 minutes?
+#
+#    How many "turnarounds" have happened during the match, i.e. how many times has a team come from a losing situation (has strictly fewer points scored than the other team) to a leading one (has strictly more points scored than the other team)?
+#
+#Input Specification
+#
+#The first line contains a positive integer A ( 1 ≤ A ≤ 2879 ) , the number of points Team A has scored. In the following A lines there are positive integers A s ( 1 ≤ A s ≤ 2880 ) , the seconds in which Team A was scoring points ordered from the smallest to the largest number.
+#
+#In the ( A + 2 ) t h line there is a positive integer B ( 1 ≤ B ≤ 2879 ) , the number of points Team B has scored. In the following B lines there are positive integers B s ( 1 ≤ B s ≤ 2880 ) , the seconds in which Team B was scoring points ordered from the smallest to the largest number.
+#Output Specification
+#
+#In the first line print an integer value, the answer to the first question from the text of the task.
+#
+#In the second line print an integer value, the answer to the second question from the text of the task.
