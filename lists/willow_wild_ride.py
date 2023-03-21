ae01e855870cc4fcd74cba3fb037fb6233335eea
VinhD <102884144+vinhds@users.noreply.github.com>
2023-03-21 11:06:32 -0500

    add willow's wild ride problem and specs

---
+++ lists/willow_wild_ride.py
@@ -0,0 +1,19 @@
+#Mandy is a working artist and is commissioned to create some art pieces for 2018.
+#The client wants Mandy to create art pieces out of cardboard boxes to display at the local art show.
+#Occasionally, Mandy plans to drive by the local store on her way home to grab some boxes for her art pieces.
+#
+#Mandy's cat, Willow, likes to play with every box that Mandy brings home. Willow plays with a box for T days before getting bored of it. Once Willow is bored with a box, she never returns to it again, meaning that Mandy can finally use the empty box in her art projects.
+#
+#If Mandy brings home another box before Willow finishes playing with the previous one,
+#Willow will wait until she is bored with the previous box before moving onto the new one.
+#
+#Given Mandy's box-shopping habits over the next N days, can you determine by how many days the project will be delayed due to Willow?
+#Input Specification
+#
+#The standard input will contain 10 datasets.
+#
+#Each dataset begins with two integers T ( 2 ≤ T ≤ 7 ) and N ( 1 ≤ N ≤ 365 ) .
+#The next N lines each contain either the letter E or B which represent whether Mandy came home empty-handed or with a box that day.
+#Output Specification
+#
+#For each dataset, output the number of days that Willow will be playing with the boxes after the N days given in the dataset.
