c127c38abcf62bad6775c106f227ae00b837ff04
VinhD <102884144+vinhds@users.noreply.github.com>
2023-03-22 11:08:41 -0500

    add free_shirts problem and specs

---
+++ lists/free_shirts.py
@@ -0,0 +1,22 @@
+#Throughout the year, there are many programming events that students can attend to meet like-minded individuals,
+#hone their skills and, most importantly, get a free t-shirt.
+#
+#Ian is an avid attender of these events because he hates doing laundry. Ian only does his laundry when all his shirts are dirty,
+#so this constant influx of shirts allows him to put off laundry for longer periods of time.
+#
+#Ian starts with N clean shirts. Ian wears one clean shirt every day, after which it becomes dirty.
+#If at the beginning of a day (before any events) Ian only has dirty shirts, then he will do the laundry,
+#which makes all his shirts clean again. If Ian goes to an event, then he will receive one clean shirt.
+#
+#Given the initial number of shirts that Ian has and the schedule of events for the next D days,
+#how many times will Ian do the laundry in the next D days?
+#Input Specification
+#
+#The input contains 10 datasets. Each dataset begins with three integers N , M , D ( 1 ≤ N , M ≤ 100 , 1 ≤ D ≤ 1 000 )
+#, the initial number of shirts that Ian has, the number of events coming up, and the number of days, respectively.
+#
+#The next line contains M integers A i ( 1 ≤ A i ≤ D ) , the days on which there are events.
+#There may be multiple events in a single day.
+#Output Specification
+#
+#For each dataset, output the number of times that Ian will do the laundry in the next D days.
