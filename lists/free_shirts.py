ec2d24091b7a177483aa46c0f1d93c4492633717
VinhD <102884144+vinhds@users.noreply.github.com>
2023-03-22 18:43:08 -0500

    solve free_shirts problem

--- lists/free_shirts.py
+++ lists/free_shirts.py
@@ -2,7 +2,7 @@
 #hone their skills and, most importantly, get a free t-shirt.
 #
 #Ian is an avid attender of these events because he hates doing laundry. Ian only does his laundry when all his shirts are dirty,
-#so this constant influx of shirts allows him to put off laundry for longer periods of time.
+
 #
 #Ian starts with N clean shirts. Ian wears one clean shirt every day, after which it becomes dirty.
 #If at the beginning of a day (before any events) Ian only has dirty shirts, then he will do the laundry,
@@ -20,3 +20,33 @@
 #Output Specification
 #
 #For each dataset, output the number of times that Ian will do the laundry in the next D days.
+
+# Get the input
+for j in range(10):
+    first_line = input()
+    n, m, d = map(int, first_line.split(' '))
+    event_days = []
+    second_line = input()
+    event_days = list(map(int, second_line.split(' ')))
+    n_clean_shirts = n
+    laundry = False
+    n_laundry = 0
+# For each day:
+# Count the number of free t-shirts based on the number of events
+# Update the total number of shirts, the number of clean shirts
+# Make the laundry decision based on the number of clean shirts
+# Keep track of the number of laundries done
+    for i in range(d):
+        n_free_shirts = event_days.count(i+1)
+        n += n_free_shirts
+        if n_clean_shirts == 0:
+            laundry = True
+            n_laundry += 1
+        elif n_clean_shirts > 0:
+            laundry = False
+        if laundry == True:
+            n_clean_shirts = n
+        elif laundry == False:
+            n_clean_shirts += n_free_shirts        
+        n_clean_shirts -= 1
+    print(n_laundry)
