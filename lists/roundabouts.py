c44f2fe6bff6de03b1920bccf9feec8b0826cd04
VinhD <102884144+vinhds@users.noreply.github.com>
2023-03-23 20:25:58 -0500

    add roundabouts problem and specs

---
+++ lists/roundabouts.py
@@ -0,0 +1,12 @@
+#Input Specification
+#
+#The standard input will contain 10 datasets. Each dataset begins with an integer N ( 2 ≤ N ≤ 700 ) , the number of routes.
+#The next N lines each contain a series of integers describing a route.
+#
+#The first integer of each route description is the ID for the route. The second integer R ( 1 ≤ R ≤ 70 )
+#is the number of roundabouts along the route. R integers follow which describe the diameter D ( 1 ≤ D ≤ 70 000 )
+#of each roundabout along the route.
+#Output Specification
+#
+#For each dataset, output the minimum roundabout diameter along a route followed by a brace-enclosed,
+#sorted list of route IDs for the routes that could cause issues.
