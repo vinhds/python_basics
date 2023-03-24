2b475d136d7f257ed890320c1059ce4750ccc1cf
VinhD <102884144+vinhds@users.noreply.github.com>
2023-03-24 12:39:15 -0500

    add problem about counting rectangles made of *

---
+++ lists/counting_rectangles.py
@@ -0,0 +1,20 @@
+#While playing in his favourite text editor, Daniel decided to draw a picture that was N characters high and M characters wide.
+#The picture consists solely of characters . and * such that characters * form some non-overlapping rectangles.
+#The rectangles don't even touch each other on their sides or corners.
+#
+#Help Daniel count the number of rectangles drawn on the picture.
+#Input
+#
+#The first line contains two integers N and M ( 1 ≤ N , M ≤ 100 ) from task description.
+#
+#Each of the next N lines contains M characters . or * which represent the picture that Daniel drew.
+#Output
+#
+#In a single line you should output the number of rectangles on the picture.
+#Scoring
+#
+#In the test cases worth a total of 20 % of the points, all rectangles will consist of a single * character.
+#
+#In the test cases worth additional 30 % of the points, it will hold N = 1 .
+
+
