7d244a33f72efce6d9afd4f7ff269d9c09611b89
VinhD <102884144+vinhds@users.noreply.github.com>
2023-04-05 08:46:28 -0500

    solve the distance between cities problem

--- functions/distance_cities.py
+++ functions/distance_cities.py
@@ -16,10 +16,14 @@
 # 1. Write a distance function that calculate dist(i,j) from city i and city j.
 # 2. Write a function to make each row of the matrix.
 # 3. Write a function to print each row.
+# This is O(n^2) with a coefficient k of kn^2 with k>=2
 
 # Get the input
 consec_distances = list(map(int, input().split(' ')))
 
+# Calculate the distance between two cities
+# Example: d[1,4] = d[1] + d[2] + d[3] where d[1] = d[1,2], d[2] = d[2,3], d[3] = d[3,4] are the consecutive distances
+# So in general if i < j: d[i,j] = d[i] + d[i+1] + ... + d[i + (j - i - 1)] where d[m] = d[m, m+1]
 def dist(i, j):
     d = 0
     if i < j:
@@ -31,4 +35,21 @@ def dist(i, j):
     elif i > j:
         d = dist(j, i)
     return d
+
+# Make any row of the matrix
+def make_row(index, n_cols):
+    row = []
+    for i in range(n_cols):
+        row.append(dist(index, i))
+    return row
+# Print any row of the matrix
+def print_row(row):
+    for item in row:
+        print(item, end=' ')
+    print('\n')
+
+# Solve the problem
+for i in range(5):
+    row = make_row(i, 5)
+    print_row(row)
 
