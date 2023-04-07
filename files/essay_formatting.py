62b15dbb578cd2ed7c58cb3dba8d51983b743816
VinhD <102884144+vinhds@users.noreply.github.com>
2023-04-06 23:27:02 -0500

    Add a folder consisting of problems that requires reading and writing files

---
+++ files/essay_formatting.py
@@ -0,0 +1,22 @@
+#Bessie the cow is writing an essay. Each word in the essay contains only low-
+#ercase or uppercase characters. Her teacher has specified the maximum
+#number of characters, not counting spaces, that can occur per line. To sat-
+#isfy this requirement, Bessie writes down the words of the essay using the
+#following rules:
+#• If the next word fits on the current line, add it to the current line.
+#Include a space between each pair of words on the line.
+#• Otherwise, put this word on a new line; this line becomes the new
+#current line.
+#Output the essay with the correct words on each line.
+#Input
+#Read input from the file named word.in.
+#The input consists of two lines.
+#• The first line contains two integers separated by a space. The first
+#integer is n, the number of words in the essay; it’s between 1 and
+#100. The second integer is k, the maximum number of characters
+#(not counting spaces) that can occur per line; it’s between 1 and 80.
+#• The second line contains n words, with a space between each pair of
+#words. Each word has at most k characters.
+#Output
+#Write output to the file named word.out.
+#Output the properly formatted essay.
